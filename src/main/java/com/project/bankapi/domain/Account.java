d134d2c3f20a0fc3f8194cbc4596a5cb2a1f6cef
Ulugbek Ishmatov <ishmatovulugbek@gmail.com>
2025-12-22 02:06:27 +0300

    Add Account JPA entity

---
+++ src/main/java/com/project/bankapi/domain/Account.java
@@ -0,0 +1,36 @@
+package com.project.bankapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = OffsetDateTime.now();
+    }
+}
+
